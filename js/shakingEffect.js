7aba46a5ea29d24033a4509f4ef6b7a641f24bd8
binarygrapes <prashant30191@gmail.com>
2018-02-12 00:20:31 +0530

    Add files via upload

--- js/shakingEffect.js
+++ js/shakingEffect.js
@@ -23,9 +23,23 @@ $(window).on('mousemove click', function(e) {
 
   var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
   var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-  lFollowX = (40 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-  lFollowY = (20 * lMouseY) / 100;
+  lFollowX = (30 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (25 * lMouseY) / 100;
 
 });
 
 moveBackground();
+
+
+$(document).ready(function() {
+var movementStrength = 40;
+var height = movementStrength / $(window).height();
+var width = movementStrength / $(window).width();
+$("#intro").mousemove(function(e){
+          var pageX = e.pageX - ($(window).width() / 2);
+          var pageY = e.pageY - ($(window).height() / 2);
+          var newvalueX = width * pageX * -1 - 25;
+          var newvalueY = height * pageY * -1 - 50;
+          $('#intro').css("background-position", newvalueX+"px     "+newvalueY+"px");
+});
+});
